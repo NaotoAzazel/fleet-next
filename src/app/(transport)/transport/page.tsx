750482acbd5c133b3053507ffc61cfae27e026a0
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-03-05 20:08:41 +0300

    added transport page

--- src/app/(transport)/transport/page.tsx
+++ src/app/(transport)/transport/page.tsx
@@ -3,6 +3,8 @@ import Header from "@/components/header";
 import SortButton from "@/components/sort-button";
 import { Input } from "@/components/ui/input";
 import { prisma } from "@/lib/prisma";
+import Filter from "@/components/filter";
+import ProductCard from "@/components/product-card";
 
 async function getPosts() {
   const posts = await prisma.transport.findMany();
@@ -13,19 +15,21 @@ export default async function TransportPage() {
   // const posts = await getPosts();
   // console.log({ posts });
 
+  const myId = 1;
+  const adminsId = new Set([1]);
+
+  const array = Array.from({ length: 10 });
+
   return (
     <MaxWidthWrapper>
       <div className="grid flex-1 my-7">
         <Header heading="Список доступного транспорта" text="Резервируйте транспорт в один клик" />
+        <Filter />
 
-        {/* <div className="flex flex-col mt-4 space-y-2 md:space-y-0 lg:space-x-2 sm:flex-row"> */}
-        <div className="mt-4 flex flex-col gap-2 sm:flex-row sm:justify-between lg:justify-normal">
-          <SortButton />
-          <Input 
-            className="w-[240px]" 
-            type="text" 
-            placeholder="Найти транспорт..."
-          />
+        <div className="my-7 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {array.map((v) => (
+            <ProductCard />
+          ))}
         </div>
       </div>
     </MaxWidthWrapper>
