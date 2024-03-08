16d39ddff4ff363b0a2be6d6bc18ef92839955ba
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-03-08 22:30:35 +0300

    fix error when next build

--- src/app/(transport)/transport/page.tsx
+++ src/app/(transport)/transport/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/header";
 import { prisma } from "@/lib/prisma";
 import Filter from "@/components/filter";
 import ProductCard from "@/components/product-card";
+import { Suspense } from "react";
 
 async function getPosts() {
   const posts = await prisma.transport.findMany();
@@ -16,7 +17,9 @@ export default function TransportPage() {
     <MaxWidthWrapper>
       <div className="grid flex-1 my-7">
         <Header heading="Список доступного транспорта" text="Резервируйте транспорт в один клик" />
-        <Filter />
+        <Suspense>
+          <Filter />
+        </Suspense>
 
         <div className="my-7 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {array.map((_, i) => (
