d909e0c2ead691bb705a4757b3609690ec15e997
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-03-16 21:14:26 +0300

    reduced sidebar width

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -6,8 +6,8 @@ export default function DashBoardLayout({ children }: { children?: React.ReactNo
   return (
     <div className="flex flex-col">
       <MaxWidthWrapper>
-        <div className="grid flex-1 gap-12 md:grid-cols-[200px_1fr] my-7">
-          <aside className="hidden w-200px flex-col md:flex">
+        <div className="grid flex-1 gap-12 md:grid-cols-[170px_1fr] my-7">
+          <aside className="hidden w-170px flex-col md:flex">
             <DashboardNav items={dashboardConfig.sidebarNav} />
           </aside>
           <main className="flex w-full flex-1 flex-col overflow-hidden">
