aa18ab83e7859d326e0495b31ae6d577a0b83a16
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-04-08 22:16:14 +0300

    changed sizes property

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -34,7 +34,7 @@ export function MyImage({
       }
       alt="Product-Image"
       loading="lazy"
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
       className={cn(
         "inset-0 object-cover rounded",
         className
